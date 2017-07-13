c0c41f67a966b5761fbc0669af49a9b578afa598
Dixi KIYINDOU <dixilindez@gmail.com>
2017-07-13 14:58:54 +0200

    Add UserSrvImpl

---
+++ src/main/java/com/scholanova/groupe2/libraryproject/services/dao/impl/UserSrvImpl.java
@@ -0,0 +1,25 @@
+package com.scholanova.groupe2.libraryproject.services.dao.impl;
+
+import java.util.Collection;
+
+import com.scholanova.groupe2.libraryproject.entities.User;
+import com.scholanova.groupe2.libraryproject.services.UserSrv;
+
+public class UserSrvImpl implements UserSrv {
+
+	public User get(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<User> getByFields(String attributName, Object... values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
