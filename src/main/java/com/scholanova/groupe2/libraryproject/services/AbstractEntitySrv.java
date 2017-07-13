e316bce82b095dba48c28b6aa612852918b10bc2
Dixi KIYINDOU <dixilindez@gmail.com>
2017-07-13 14:54:32 +0200

    Add abstractEntitySrv

---
+++ src/main/java/com/scholanova/groupe2/libraryproject/services/AbstractEntitySrv.java
@@ -0,0 +1,28 @@
+package com.scholanova.groupe2.libraryproject.services;
+
+import java.util.Collection;
+
+import com.scholanova.groupe2.libraryproject.entities.AbstractEntity;
+
+public interface AbstractEntitySrv <T extends AbstractEntity> {
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public T get(Long id);
+	
+	/**
+	 * 
+	 * @param attributName
+	 * @param values
+	 * @return
+	 */
+	public Collection<T> getByFields (String attributName, Object ... values);
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Collection<T> getAll();
+}
