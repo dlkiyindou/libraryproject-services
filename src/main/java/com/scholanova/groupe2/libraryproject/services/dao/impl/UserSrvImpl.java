a677e0b8fb00e4ce3bd42598604c1072fb51552d
sego <sego.maris@gmail.com>
2017-07-18 20:05:27 +0200

    a

--- src/main/java/com/scholanova/groupe2/libraryproject/services/dao/impl/UserSrvImpl.java
+++ src/main/java/com/scholanova/groupe2/libraryproject/services/dao/impl/UserSrvImpl.java
@@ -2,14 +2,17 @@ package com.scholanova.groupe2.libraryproject.services.dao.impl;
 
 import java.util.Collection;
 
+import org.hibernate.Session;
+
 import com.scholanova.groupe2.libraryproject.entities.User;
 import com.scholanova.groupe2.libraryproject.services.UserSrv;
+import com.scholanova.groupe2.libraryproject.services.utils.HibernateUtil;
 
 public class UserSrvImpl implements UserSrv {
+	private Session session = HibernateUtil.getSession();
 
 	public User get(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return session.load(User.class, id);
 	}
 
 	public Collection<User> getByFields(String attributName, Object... values) {
@@ -18,8 +21,7 @@ public class UserSrvImpl implements UserSrv {
 	}
 
 	public Collection<User> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return session.createQuery("FROM User", User.class).getResultList();
 	}
 
 }
