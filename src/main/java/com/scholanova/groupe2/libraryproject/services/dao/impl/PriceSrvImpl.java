840386e41d6df0fd8039aa4dc556f436faec2e56
Yoann <yoann.farre@gmail.com>
2017-07-19 18:17:26 +0200

    commit for switch

---
+++ src/main/java/com/scholanova/groupe2/libraryproject/services/dao/impl/PriceSrvImpl.java
@@ -0,0 +1,25 @@
+package com.scholanova.groupe2.libraryproject.services.dao.impl;
+
+import java.util.Collection;
+
+import com.scholanova.groupe2.libraryproject.entities.Price;
+import com.scholanova.groupe2.libraryproject.services.PriceSrv;
+
+public class PriceSrvImpl implements PriceSrv {
+
+	public Price get(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Price> getByFields(String attributName, Object... values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Price> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
